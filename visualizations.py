fce532a6dd95b274a913306e18967d99f15d7925
Svetoslav Paregov <paregov@gmail.com>
2019-06-08 19:08:44 +0300

    - Added new function for visualization. It shows the images bigger and on multiple rows. It is designed for better compare.

--- visualizations.py
+++ visualizations.py
@@ -24,37 +24,119 @@ def plot_images_for_compare_separate(inputs, predictions, truths, indexes):
         plt.show()
 
 
-def plot_multi_model_images_for_compare_separate(inputs, predictions, truths, indexes, show_input=False):
+def plot_multi_model_images_for_compare_separate(inputs, predictions, model_names, truths, indexes, show_input=False, show_interpolated=True):
     length = len(indexes)
     predictions_count = len(predictions)
 
+    width = predictions_count + 1
+    if show_input:
+        width += 1
+        
+    if show_interpolated:
+        width += 1
+    
     for i in range(length):
         pos = 0
-        f, axarr = plt.subplots(1, predictions_count + 2, figsize=(25, 10))
+        f, axarr = plt.subplots(1, width, figsize=(25, 10))
         img_index = indexes[i]
         if show_input:
             axarr[pos].imshow(inputs[img_index])
+            axarr[pos].set_title('input')
+            pos += 1
+        
+        if show_input:
+            axarr[pos].imshow(inputs[img_index], interpolation='bicubic')
+            axarr[pos].set_title('bicubic')
             pos += 1
 
+        index = 0
         for prediction in predictions:
             axarr[pos].imshow((prediction[img_index]*255).astype(np.uint8))
+            axarr[pos].set_title(model_names[index])
             pos += 1
+            index += 1
 
         axarr[pos].imshow((truths[img_index]*255).astype(np.uint8))
+        axarr[pos].set_title('truth')
         plt.show()
 
 
-def compare_models(test_data_tensors, test_truth_tensors, models_data, test_image_index_to_show, show_input=False):
-    models = []
+def plot_single_image_multi_model(input_image, predictions, truth, show_input=True, show_interpolated=True):
+    """
+    Plot a single image that is predicted with multiple models for compare.
+    We are going to show 4 images per row and will create as many rows as needed to show all of the predictions, input, interpolated and truth
+    """
+    images_per_row = 4
+    predictions_count = len(predictions)
+
+    total_count = predictions_count + 1
+    if show_input:
+        total_count += 1
+        
+    if show_interpolated:
+        total_count += 1
+    
+    rows = int(total_count / images_per_row) + 1
+    
+    prediction_index = 0
+    for i in range(rows):
+        pos = 0
+        f, axarr = plt.subplots(1, images_per_row, figsize=(25, 5))
+
+        if i == 0 and show_input:
+            axarr[pos].imshow(input_image)
+            axarr[pos].set_title('input')
+            pos += 1
+        
+        if i == 0 and show_input:
+            axarr[pos].imshow(input_image, interpolation='bicubic')
+            axarr[pos].set_title('bicubic')
+            pos += 1
+
+        if i == 0:
+            axarr[pos].imshow((truth*255).astype(np.uint8))
+            axarr[pos].set_title('truth')
+            pos += 1
+
+        for p in range(predictions_count):
+            prediction = predictions[prediction_index]
+            axarr[pos].imshow((prediction['image']*255).astype(np.uint8))
+            axarr[pos].set_title(prediction['name'])
+            pos += 1
+            prediction_index += 1
+            
+            if pos >= images_per_row or prediction_index >= predictions_count:
+                break
+            
+        plt.show()
+
+
+def compare_models(test_data_tensors, test_truth_tensors, models_data, test_image_index_to_show, show_input=True, show_interpolated=True):
+    predictions = []
+    model_names = []
+    for model_data in models_data:
+        model_names.append(model_data['name'])
+        model_data['model'].load_weights(model_data['checkpoint'])
+        predictions.append(model_data['model'].predict(test_data_tensors))
+    
+    print('Plotting the results...')
+    plot_multi_model_images_for_compare_separate(test_data_tensors, predictions, model_names, test_truth_tensors, test_image_index_to_show, show_input)
+
+    print('All done!')
+
+    
+def compare_models_single_iamge(test_data_tensors, test_truth_tensors, models_data, test_image_index_to_show, show_input=True, show_interpolated=True):
+    predictions_per_model = []
     for model_data in models_data:
         model_data['model'].load_weights(model_data['checkpoint'])
-        models.append(model_data['model'])
+        predictions_per_model.append({'data': model_data['model'].predict(test_data_tensors), 'name': model_data['name']})
     
-    predictions = []
-    for model in models:
-        predictions.append(model.predict(test_data_tensors))
+    for i in test_image_index_to_show:
+        predictions = []
+        for pred in predictions_per_model:
+            predictions.append({'image': pred['data'][i], 'name': pred['name']})
 
-    print('Plotting the results...')
-    plot_multi_model_images_for_compare_separate(test_data_tensors, predictions, test_truth_tensors, test_image_index_to_show, show_input)
+        print('Plotting the results...')
+        plot_single_image_multi_model(test_data_tensors[i], predictions, test_truth_tensors[i])
 
     print('All done!')
